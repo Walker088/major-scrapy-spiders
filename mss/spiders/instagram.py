f64ac36e781bec90ca6974a5e7c8a4665567450f
Walker088 <hanxu2121@gmail.com>
2017-12-11 10:10:11 +0800

    modify ig scraper to save json earlier

--- mss/spiders/instagram.py
+++ mss/spiders/instagram.py
@@ -8,10 +8,15 @@ class Instagram(Spider):
     '''MSS: Instagram Spider'''
     name = 'Instagram'
     start_urls = [
-        'https://www.instagram.com/nike/',
+        'https://www.instagram.com/onionman__/',
     ]
 
     def parse(self, response):
         '''Parse profile JSON data'''
         json_string = response.xpath('//script/text()').re(r'sharedData = (.+);')[0]
+        self.saveJson(json.loads(json_string))
         return json.loads(json_string)
+    
+    def saveJson(self, jsonStr):
+        with open ('../data/instagram.json','w') as f:
+            json.dump(jsonStr,f)
